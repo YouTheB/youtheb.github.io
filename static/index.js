c8a0eede3b0fd02eb09ace61ab05fa551d957175
YouTheB <41529853+YouTheB@users.noreply.github.com>
2022-01-27 16:08:47 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -247,20 +247,16 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
 
     let _ttreg = / t{1,2}(\d+)/,
         _clearttClsReg = / t{1,2}\d+| bad/;
-    let last=1;
     let right=1;
+    let last=0;
     function refreshGameLayer(box, loop, offset) {
-        let i = last + (loop ? 0 : 4);
         if(right == 1)
-        {
-            last=Math.floor(Math.random()*1000)%(4-last)+last;
-            right=0;
-        }
+            last=Math.floor(Math.random()*1000)%2+2;
         else
-        {
-            last=Math.floor(Math.random()*1000)%(last);
-            right=1;
-        }
+            last=Math.floor(Math.random()*1000)%2;
+        right=1-right;
+        let i = last + (loop ? 0 : 4);
+        
         for (let j = 0; j < box.children.length; j++) {
             let r = box.children[j], rstyle = r.style;
             rstyle.left = (j % 4) * blockSize + 'px';
@@ -275,17 +271,12 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
                 });
                 r.className += ' t' + (Math.floor(Math.random() * 1000) % 5 + 1);
                 r.notEmpty = true;
-                i = (Math.floor(j / 4) + 1) * 4 + last;
                 if(right == 1)
-                {
-                    last=Math.floor(Math.random()*1000)%(4-last)+last;
-                    right=0;
-                }
+                    last=Math.floor(Math.random()*1000)%2+2;
                 else
-                {
-                    last=Math.floor(Math.random()*1000)%(last);
-                    right=1;
-                }
+                    last=Math.floor(Math.random()*1000)%2;
+                right=1-right;
+                i = (Math.floor(j / 4) + 1) * 4 + last;
             } else {
                 r.notEmpty = false;
             }
